eef9af41765e45378ada31a070ee752c3c665cb1
Valeria <lera411097@gmail.com>
2020-09-10 18:35:04 +0300

    commit

---
+++ flex/index.js
@@ -0,0 +1,67 @@
+calc.one.addEventListener('click', function() {
+    calc.result.value += '1';
+}, false)
+
+calc.two.addEventListener('click', function() {
+    calc.result.value += '2';
+}, false)
+
+calc.three.addEventListener('click', function() {
+    calc.result.value += '3';
+}, false)
+
+calc.four.addEventListener('click', function() {
+    calc.result.value += '4';
+}, false)
+
+calc.five.addEventListener('click', function() {
+    calc.result.value += '5';
+}, false)
+
+calc.six.addEventListener('click', function() {
+    calc.result.value += '6';
+}, false)
+
+calc.seven.addEventListener('click', function() {
+    calc.result.value += '7';
+}, false)
+
+calc.eight.addEventListener('click', function() {
+    calc.result.value += '8';
+}, false)
+
+calc.nine.addEventListener('click', function() {
+    calc.result.value += '9';
+}, false)
+
+calc.zero.addEventListener('click', function() {
+    calc.result.value += '0';
+}, false)
+
+calc.AC.addEventListener('click', function() {
+    calc.result.value = ' ';
+}, false)
+
+calc.coma.addEventListener('click', function() {
+    calc.result.value += '.';
+}, false)
+
+calc.plus.addEventListener('click', function() {
+    calc.result.value += '+';
+}, false)
+
+calc.minus.addEventListener('click', function() {
+    calc.result.value += '-';
+}, false)
+
+calc.multiply.addEventListener('click', function() {
+    calc.result.value += '*';
+}, false)
+
+calc.split.addEventListener('click', function() {
+    calc.result.value += '/';
+}, false)
+
+calc.equal.addEventListener('click', function() {
+    calc.result.value = eval(calc.result.value);
+}, false)
